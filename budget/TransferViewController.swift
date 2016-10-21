b613bc4d1d0f2316696b46847ceb504dece5a3dc
azamrahmatali <azam@brainloadtech.com>
2016-10-21 18:24:55 +0500

    transfer controllerr added

---
+++ budget/TransferViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TransferViewController.swift
+//  budget
+//
+//  Created by Azam Rahmat on 10/21/16.
+//  Copyright © 2016 Brainload Technologies. All rights reserved.
+//
+
+import UIKit
+
+class TransferViewController: UIViewController {
+    @IBOutlet weak var date: UITextField!
+
+    @IBOutlet weak var doThisBefore: UILabel!
+    @IBOutlet weak var amount: UITextField!
+    @IBOutlet weak var toAccount: UITextField!
+    @IBOutlet weak var fromAccount: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
